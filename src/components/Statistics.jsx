3abc6826a08d2d2b9150c1851d5e89a3420403c6
Konurbaeva <konurbaevamadina@gmail.com>
2022-04-30 21:27:03 +0200

    added %

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -14,19 +14,19 @@ export const Statistics = ({title, stats}) => {
   <ul className="stat-list">
     <li className="item">
       <span className="label">{stats[0].label}</span>
-      <span className="percentage">{stats[0].percentage}</span>
+      <span className="percentage">{stats[0].percentage} %</span>
     </li>
     <li className="item">
       <span className="label">{stats[1].label}</span>
-      <span className="percentage">{stats[1].percentage}</span>
+      <span className="percentage">{stats[1].percentage} %</span>
     </li>
     <li className="item">
       <span className="label">{stats[2].label}</span>
-      <span className="percentage">{stats[2].percentage}</span>
+      <span className="percentage">{stats[2].percentage} %</span>
     </li>
     <li className="item">
       <span className="label">{stats[3].label}</span>
-      <span className="percentage">{stats[3].percentage}</span>
+      <span className="percentage">{stats[3].percentage} %</span>
     </li>
   </ul>
 </section>
