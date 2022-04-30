e9cee5a29c77fd9d486898a118c12d3413704b7e
Konurbaeva <konurbaevamadina@gmail.com>
2022-04-30 21:13:23 +0200

    Statistics init

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,35 @@
+//   [
+//     { "id": "id-1", "label": ".docx", "percentage": 22 },
+//     { "id": "id-2", "label": ".pdf", "percentage": 4 },
+//     { "id": "id-3", "label": ".mp3", "percentage": 17 },
+//     { "id": "id-4", "label": ".psd", "percentage": 47 },
+//     { "id": "id-5", "label": ".pdf", "percentage": 10 }
+//   ]
+
+export const Statistics = ({title, stats}) => {
+    return (
+    <section class="statistics">
+  {title? <h2 class="title">Upload stats</h2>: null}
+
+  <ul class="stat-list">
+    <li class="item">
+      <span class="label">{stats[0].label}</span>
+      <span class="percentage">{stats[0].percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">{stats[1].label}</span>
+      <span class="percentage">{stats[1].percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">{stats[2].label}</span>
+      <span class="percentage">{stats[2].percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">{stats[3].label}</span>
+      <span class="percentage">{stats[3].percentage}</span>
+    </li>
+  </ul>
+</section>
+    );
+  };
+
