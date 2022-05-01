0a58eadbcc9fd4a7886f36c3396e218fc4b0322f
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-01 11:47:40 +0200

    added  FriendListItem.propTypes

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,11 +1,20 @@
+import PropTypes from 'prop-types';
+
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (
        <div>
         {/* <span className="status">{isOnline? 'Yes' : 'No' }</span> */}
-
         <span className="status">{isOnline && 'Yes'}</span>
         <img className="avatar" src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
         </div>
     );
   }; 
+
+  FriendListItem.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired
+    })).isRequired
+  }
