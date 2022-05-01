4ac161079fe4e38a48608792c111b8261da4ff2a
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-01 11:32:39 +0200

    refactored Statistics component

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,39 +1,21 @@
-//   [
-//     { "id": "id-1", "label": ".docx", "percentage": 22 },
-//     { "id": "id-2", "label": ".pdf", "percentage": 4 },
-//     { "id": "id-3", "label": ".mp3", "percentage": 17 },
-//     { "id": "id-4", "label": ".psd", "percentage": 47 },
-//     { "id": "id-5", "label": ".pdf", "percentage": 10 }
-//   ]
 import PropTypes from 'prop-types';
 
-export const Statistics = ({title,
-   stats: { label, percentage}}) => {
-    return (
-    <section className="statistics">
-  {title && <h2 className="title">{title}</h2>}
+export const Statistics = ({title, stats}) => {
+  return (
+  <section className="statistics">
+{title && <h2 className="title">{title}</h2>}
 
-  <ul className="stat-list">
-    <li className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage} %</span>
-    </li>
-    <li className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage} %</span>
-    </li>
-    <li className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage} %</span>
-    </li>
-    <li className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage} %</span>
-    </li>
-  </ul>
+<ul className="stat-list">
+  {stats.map(stat => (
+  <li className="item" key={stat.id}>
+  <span className="label">{stat.label}</span>
+  <span className="percentage">{stat.percentage} %</span>
+</li>
+  ))}
+</ul>
 </section>
-    );
-  };
+  );
+};
 
   Statistics.propTypes = {
     title: PropTypes.string.isRequired,
