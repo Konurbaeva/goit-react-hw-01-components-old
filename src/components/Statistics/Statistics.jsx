97ad12780c113dfeaac470f6bbb6ed10c0cdd3af
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-02 11:12:34 +0200

    fixed propTypes of Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -20,5 +20,9 @@ export const Statistics = ({title, stats}) => {
 
   Statistics.propTypes = {
     title: PropTypes.string.isRequired,
-    stats: PropTypes.object.isRequired
+    stats: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number,
+    })).isRequired,
   }
