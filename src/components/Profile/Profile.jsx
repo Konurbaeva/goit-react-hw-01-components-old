b71a01043bce32b70136d4c5c34f261ffa567844
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-02 10:40:28 +0200

    fix 1.review

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-export const Profile = ({username, tag, location, avatar, stats}) => {
+export const Profile = ({username, tag, location, avatar, stats: {followers, views, likes}}) => {
     return (
        <div className={styles.profile}>
       <div className={styles.description}>
@@ -18,15 +18,15 @@ export const Profile = ({username, tag, location, avatar, stats}) => {
         <ul className="stats">
           <li>
             <span className="label">Followers</span>
-            <span className="quantity">{stats.followers}</span>
+            <span className="quantity">{followers}</span>
           </li>
           <li>
             <span className="label">Views</span>
-            <span className="quantity">{stats.views}</span>
+            <span className="quantity">{views}</span>
           </li>
           <li>
             <span className="label">Likes</span>
-            <span className="quantity">{stats.likes}</span>
+            <span className="quantity">{likes}</span>
           </li>
         </ul>
       </div>
