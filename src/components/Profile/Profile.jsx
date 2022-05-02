209c4a776033e38a813597cf7bfa2039628bd7bd
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-02 10:44:34 +0200

    структуру об'єкта слід розкрити

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -37,7 +37,9 @@ export const Profile = ({username, tag, location, avatar, stats: {followers, vie
   Profile.propTypes = {
     username: PropTypes.string.isRequired,
     tag: PropTypes.string.isRequired,
-     location:PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-      stats: PropTypes.object.isRequired
+    location:PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
   }
