3820698d9cf917bf73ffcfaf660ea6212fdc674c
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-02 12:23:26 +0200

    init styled components

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,9 +1,33 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+
+/* display: flex;
+-webkit-box-align: center;
+align-items: center;
+padding: 8px;
+margin-bottom: 10px;
+border: 1px solid rgb(85, 123, 131);
+border-radius: 5px;
+ */
+
+
+const Span = styled.span`
+font-size: 1.5em;
+text-align: center;
+background-color:yellow;
+`;
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (
        <div>
-        <span className="status">{isOnline && 'Yes'}</span>
+       {/*  <span className="status">{isOnline && 'Yes'}</span> */}
+
+ {/*       {isOnline &&   <span className="status"></span>} */}
+
+ <Span>
+ {isOnline &&  <span className="status">Online</span>}
+ </Span>
         <img className="avatar" src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
         </div>
